5f01ae69af43ecb69aaa7f83345d7fa92582d8ee
Martin Met <tabordamet@gmail.com>
2021-05-16 22:13:21 -0300

    clase1
    ejercicios JS de la clase 1 de coderhood

---
+++ ejercicios.js
@@ -0,0 +1,13 @@
+///////Ejercicios clase 1///////
+function lasGalletitasDeEma() {
+
+    console.log('Receta: 3 huevos / 1 barra de chocolate / 0.5 kilos de harina/ 5 cucharadas de azucar')
+
+    let Huevos = parseInt(prompt("cantidad de huevos en la heladera: "));
+    let Chocolate = parseInt(prompt("cantidad de huevos en la heladera: "));
+    let harina = parseInt(prompt("cantidad de huevos en la heladera: "));
+    let azucar = prompt('azucar suficiente para 5 cucharadas? S/N: ');
+
+}
+
+lasGalletitasDeEma()
